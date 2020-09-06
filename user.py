4c6d5be6b30f61007f73c47b63dfa42dc7f39c1d
rickymwethera <rickymwethera@gmail.com>
2020-09-06 13:34:02 +0300

    added user class

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    """new users creation"""
+    user_list = []
+
+    def __init__(self,username, password, platform):
+        self.username = username
+        self.password = password
+        self.platform = platform
+
+
+    def save_user(self):
+        """add user into user_list"""
+        User.user_list.append(self)
